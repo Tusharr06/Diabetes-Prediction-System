94416df2467a0249bde0cca711075f8d4bfed147
Tushar <tusharcodes13@gmail.com>
2024-12-24 14:14:00 +0530

    Standardized The Features And Preprocessed The Diabetes Dataset.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import numpy as np 
-import pandas as pd 
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-import numpy as np 
-import pandas as pd 
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+diabetes_dataset = pd.read_csv("/home/tushar/Documents/MLPROJECT-1/diabetes.csv")
+
+print(diabetes_dataset.head())
+print(diabetes_dataset.describe())
+print(diabetes_dataset["Outcome"].value_counts())
+print(diabetes_dataset.groupby("Outcome").mean())
+X = diabetes_dataset.drop(columns = "Outcome",axis=1)
+Y = diabetes_dataset["Outcome"]
+print(X)
+print(Y)
+scaler = StandardScaler()
+standarized_data = scaler.fit_transform(X)
+print(standarized_data)
+X = standarized_data
